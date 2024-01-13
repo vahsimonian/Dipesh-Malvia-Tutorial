119fc2759e58fb01e390d37594c8b80192ba7de0
vahsimonian <simonianvah@gmail.com>
2024-01-13 12:42:49 +0700

    commit

---
+++ async.js
@@ -0,0 +1,12 @@
+console.log('start operation');
+
+function sleep(miliseconds) {
+  console.log('operation is running');
+  setTimeout(() => {
+    console.log('operation is done');
+  }, miliseconds);
+}
+
+sleep(1000);
+
+console.log('do something else..');
