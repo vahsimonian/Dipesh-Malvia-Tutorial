0c77a9070b813e0a826bb852243c9d0535b1947c
vahsimonian <simonianvah@gmail.com>
2024-01-13 19:12:04 +0700

    commit

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
-function asyncTask() {
-  return Promise.resolve();
-}
+const makeApiCall = (time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This API executed in: ' + time);
+    }, time);
+  });
+};
 
-asyncTask().then(() => console.log(name));
+const multiApiCall = [
+  makeApiCall(1000),
+  makeApiCall(2000),
+  makeApiCall(3000),
+  makeApiCall(4000),
+];
 
-const name = 'Vah';
+Promise.all(multiApiCall).then((values) => {
+  console.log(values);
+});
