4af1fb10aaca96b9486f4fa192533395da30a34c
vahsimonian <simonianvah@gmail.com>
2024-01-13 17:38:14 +0700

    commit

--- callback.js
+++ callback.js
@@ -1,16 +1,8 @@
-console.log('Task start');
+//
+
 function asyncTask(cb) {
   console.log('Task is running');
   setTimeout(() => {
     cb(null, 'Data from asyncFunction');
   });
 }
-asyncTask((err, data) => {
-  if (err) {
-    throw err;
-  } else {
-    console.log('data', data);
-  }
-});
-console.log('Task end');
-const name = 'Vah';
