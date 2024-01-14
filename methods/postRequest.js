14e82e830b8ad1563b812e1420fe544aefeba389
vahsimonian <simonianvah@gmail.com>
2024-01-14 16:02:16 +0700

    commit

--- methods/postRequest.js
+++ methods/postRequest.js
@@ -1,29 +1,22 @@
 const crypto = require('crypto');
-
 const requestBodyParser = require('../utilities/body-parser');
-
 module.exports = async (req, res) => {
   if (req.url === '/api/movies') {
     try {
       let body = await requestBodyParser(req);
       body.id = crypto.randomUUID();
       req.movies.push(body);
-      res.wrireHead(201, { 'Content-Type': 'application/json' });
+      res.writeHead(201, { 'Content-Type': 'application/json' });
       res.end();
     } catch (error) {
-      console.log(error),
-        res.writeHead(400, { 'Content-Type': 'application/json' });
-
-      JSON.stringify({
-        title: 'Validation failed',
-        message: 'UUID is not valid',
-      }),
-        res.end(
-          JSON.stringify({
-            title: 'Validation failed',
-            message: 'UUID is not valid',
-          })
-        );
+      console.log(error);
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({
+          title: 'Validation failed',
+          message: 'UUID is not valid',
+        })
+      );
     }
   }
 };
