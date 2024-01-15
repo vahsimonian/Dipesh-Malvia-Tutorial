b2ba3c0a7edf596e2b35792c12b02885a8917461
vahsimonian <simonianvah@gmail.com>
2024-01-15 12:24:36 +0700

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.end('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
