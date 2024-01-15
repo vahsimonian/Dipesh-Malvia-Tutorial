022b51a03cc3987658cf5473469c6e0de076c704
vahsimonian <simonianvah@gmail.com>
2024-01-15 09:49:38 +0700

    commit

--- methods/postRequest.js
+++ methods/postRequest.js
@@ -1,11 +1,13 @@
 const crypto = require('crypto');
 const requestBodyParser = require('../utilities/body-parser');
+const writeToFile = require('../utilities/write-to-file');
 module.exports = async (req, res) => {
   if (req.url === '/api/movies') {
     try {
       let body = await requestBodyParser(req);
       body.id = crypto.randomUUID();
       req.movies.push(body);
+      writeToFile(req.movies);
       res.writeHead(201, { 'Content-Type': 'application/json' });
       res.end();
     } catch (error) {
@@ -18,5 +20,8 @@ module.exports = async (req, res) => {
         })
       );
     }
+  } else {
+    res.writeHead(404, 'Content-Type', 'application/json');
+    res.end(JSON.stringify({ title: 'Not found', message: 'Route not found' }));
   }
 };
---
+++ utilities/write-to-file.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (data) => {
+  console.log('data to write');
+
+  try {
+    fs.writeFileSync(
+      path.join(__dirname, '..', 'data', 'movies.json'),
+      JSON.stringify(data),
+      'utf-8'
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
