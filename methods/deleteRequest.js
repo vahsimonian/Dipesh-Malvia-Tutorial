811b1a74d3819a5e4f343b25727111365321c027
vahsimonian <simonianvah@gmail.com>
2024-01-15 09:57:49 +0700

    commit

--- methods/deleteRequest.js
+++ methods/deleteRequest.js
@@ -1,1 +1,14 @@
-module.exports = (req, res) => {};
+module.exports = (req, res) => {
+  let baseUrl = req.url.substring(0, req.url.lastOfIndex('/') + 1);
+  let id = new RegExp('//vecd//');
+
+  if (regexV4.test(id)) {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(
+      JSON.stringify({
+        title: 'Validation Failed',
+        message: 'UUID is not valid',
+      })
+    );
+  }
+};
