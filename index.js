204f7ae6de40e9423e9642e15fbd98943040895a
vahsimonian <simonianvah@gmail.com>
2024-01-16 18:31:05 +0700

    commit

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
-  res.end('Hello World');
+  res.json({ message: 'This is home page' });
+});
+
+app.get('/users/:id', (req, res) => {
+  res.json({ message: `Get user with ID ${req.params.id}` });
+});
+
+app.post('/users/:id', (req, res) => {
+  res.json({ message: `Create new user` });
+});
+
+app.put('/users/:id', (req, res) => {
+  res.json({ message: `Update user with ID ${req.params.id}` });
+});
+
+app.delete('/users/:id', (req, res) => {
+  res.json({ message: `Delete user with ID ${req.params.id}` });
 });
 
 app.listen(port, () => {
